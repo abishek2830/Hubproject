c4d1e4e66eb99ab1991d47d70aa5856baddbe8d1
abishek2830 <l.s.abishek2021@gmail.com>
2025-03-12 14:44:18 +0530

    program'

---
+++ returntype/multiple.java
@@ -0,0 +1,16 @@
+public class multiple {
+    public int multiple(int num1,int num2){
+     int num3=num3=num1*num2;
+     return num3;
+ 
+    }
+    public static void main(String[] args) {
+     multiple m =new multiple();
+     m.multiple(45,23);
+     int z=m.multiple(45,23);
+     System.out.println(z);
+        
+    }
+     
+ }
+ 
