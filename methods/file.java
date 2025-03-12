db96033bcf0562d83608256d0e84d9e48f1144ee
abishek2830 <l.s.abishek2021@gmail.com>
2025-03-12 14:30:38 +0530

    program

---
+++ methods/file.java
@@ -0,0 +1,15 @@
+public class file {
+    public static void add(int num1,int num2){
+        int num3=num1+num2;
+        System.out.println(num3);
+    }
+    public static void main(String args[]){
+        file a=new file();
+        a.add(23,25);
+    }
+}
+
+
+
+    
+
