c9640cb444daa96683cf348fcb725740c8381faf
abishek2830 <l.s.abishek2021@gmail.com>
2025-03-13 14:25:59 +0530

    program

---
+++ Controlstatement/file.java
@@ -0,0 +1,11 @@
+package Controlstatement;
+
+public class file {
+    public static void main(String[] args) {
+        for(int i=30;i<40++){
+            if(i%2==1){
+                System.err.println(i);
+            }
+        }
+    }    
+}
---
+++ LeapYear/file.java
@@ -0,0 +1,12 @@
+package LeapYear;
+
+public class file {
+    public static void main(String[] args) {
+        int year=2024;
+        if(year%4==0);
+        System.out.println("Leap year");
+        else{
+            System.out.println("Not a Leap year");
+        }
+    }
+}
