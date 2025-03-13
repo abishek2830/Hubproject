375ffdd6ce4a56411ceab08d708090b1789ceba2
abishek2830 <l.s.abishek2021@gmail.com>
2025-03-13 10:17:08 +0530

    program

---
+++ Double/File.java
@@ -0,0 +1,8 @@
+public class File {
+    public static void main (String args[])
+    {
+        double num=1.587485;
+        System.out.println(num);
+    }
+    
+}
---
+++ StringBuilder/Strbuild.java
@@ -0,0 +1,11 @@
+public  class Strbuild {
+    public static void main(String[] args)
+     {
+        String a="Hello";
+
+        String b="World";
+        
+    System.out.println(a+b);
+    }
+    
+}
