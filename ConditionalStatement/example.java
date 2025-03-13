8772e5ff7291d354e7a754508076dab051c7be17
abishek2830 <l.s.abishek2021@gmail.com>
2025-03-13 10:55:16 +0530

    program

---
+++ ConditionalStatement/example.java
@@ -0,0 +1,11 @@
+public class example {
+    public static void main(String[] args) {
+        if (true) {
+            System.out.println("you are eligible to vote");
+        }
+        if (false){
+            System.out.println("you are not eligible to vote");
+         }
+    }
+    
+}
