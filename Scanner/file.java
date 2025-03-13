542829a708350136b24e207cb06c6d0a5b0c712c
abishek2830 <l.s.abishek2021@gmail.com>
2025-03-13 15:14:18 +0530

    program

---
+++ Scanner/file.java
@@ -0,0 +1,10 @@
+package Scanner;
+import java.util.*;
+public class file {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your Age");
+        int age = input.nextInt();
+        System.out.println(age);
+    }
+}
