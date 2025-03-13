f0621284d6fc8d800c9e08d522caf56ad769d6e4
abishek2830 <l.s.abishek2021@gmail.com>
2025-03-13 13:47:04 +0530

    program

---
+++ MultiDimensionalArray/example.java
@@ -0,0 +1,9 @@
+package MultiDimensionalArray;
+
+public class example {
+    public static void main(String[] args) {
+        int arr[][]={{10,20,30},{40,50,60}};
+        System.out.println(arr[1][2]);
+        System.out.println(arr[1][0]);
+    }
+}
