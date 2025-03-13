bdd0a56f236ae3695ec0a87e805a60d1bdee2f51
abishek2830 <l.s.abishek2021@gmail.com>
2025-03-13 10:23:07 +0530

    program

---
+++ ArithmeticOperator/Operator.java
@@ -0,0 +1,11 @@
+public class Operator {
+    public static void main(String[] args) {
+        System.out.println(10+18);
+        System.out.println(30-2);
+        System.out.println(5*3);
+        System.out.println(10/2);
+        System.out.println(10%5);
+        
+    }
+    
+}
