72e2fa468e2865dadb995e372fce501538efb5d3
abishek2830 <l.s.abishek2021@gmail.com>
2025-03-13 11:03:14 +0530

    program

---
+++ ConditionalStatement/example1.java
@@ -0,0 +1,13 @@
+package Hubproject.ConditionalStatement;
+
+public class example1 {
+    public static void main(String[] args) { 
+        if (true) {
+            System.out.println("You are good");    
+        }
+        else{
+            System.out.println("You are not good");
+        }
+    }
+    
+}
