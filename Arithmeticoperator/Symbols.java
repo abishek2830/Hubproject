184b1a4cb5db077102969542b4179759c512fcad
abishek2830 <l.s.abishek2021@gmail.com>
2025-03-13 12:13:27 +0530

    program

--- Arithmeticoperator/Symbols.java
+++ Arithmeticoperator/Symbols.java
@@ -1,4 +1,5 @@
-public class Operator {
+package Hubproject.Arithmeticoperator;
+public class Symbols{
     public static void main(String[] args) {
         System.out.println(10+18);
         System.out.println(30-2);
---
+++ MajorMinorFinder/example.java
@@ -0,0 +1,13 @@
+public class example {
+    public static void main(String[] args) {
+        
+    int age=25;
+    if(age<18){
+        System.out.println("you are eligible to vote");
+    }
+    else{
+        System.out.println("you are not eligible to vote");
+    }
+    }
+    
+}
