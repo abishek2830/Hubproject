93925b0e9fc256ecab824970d8bb976afaf5d953
abishek2830 <l.s.abishek2021@gmail.com>
2025-03-13 10:31:36 +0530

    program

---
+++ Conditionaloperator/File2.java
@@ -0,0 +1,12 @@
+public class File2{
+    public static void main(String[] args) {
+        System.out.println(10>18);
+        System.out.println(30>=2);
+        System.out.println(5<3);
+        System.out.println(10>2);
+        System.out.println(10==10);
+        System.out.println(3<=20);
+        
+    }
+    
+}
