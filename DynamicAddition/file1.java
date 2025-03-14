8e1db1e77e6fcbee5e0c435b7104f35ae5c834e1
abishek2830 <l.s.abishek2021@gmail.com>
2025-03-14 09:41:20 +0530

    program'

---
+++ DynamicAddition/file1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class file1 {
+  public static void main(String[] args) {
+    Scanner input=new Scanner(System.in);   
+      System.out.println("Enter the number");
+        int num1=input.nextInt();
+        int num2=input.nextInt();
+        int num3=num1+num2;
+        System.out.println(num3);
+    }
+}
