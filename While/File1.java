f8b2768d9e10940bfc4971e5a1968cc714f5935a
abishek2830 <l.s.abishek2021@gmail.com>
2025-03-14 10:06:02 +0530

    program'

---
+++ While/File1.java
@@ -0,0 +1,10 @@
+public class File1 {
+    public static void main(String[] args) {
+        int i =91;
+        while(i<100){
+            System.err.println(i);
+            i=i+1;
+        }
+    }
+    
+}
