07060b43080c8dce4a337a0d978f1bb24939110d
abishek2830 <l.s.abishek2021@gmail.com>
2025-03-14 12:00:58 +0530

    program'

---
+++ Length/File1.java
@@ -0,0 +1,10 @@
+public class File1{
+    public static void main(String[] args) {
+        String name ="hello world";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+    }
+
+}
+    
+
