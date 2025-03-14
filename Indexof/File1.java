231792797268314d94a179e7cf3c56b1516cf59e
abishek2830 <l.s.abishek2021@gmail.com>
2025-03-14 12:06:50 +0530

    program'

---
+++ Indexof/File1.java
@@ -0,0 +1,11 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+public class File1 {
+    public File1() {
+    }
+ 
+    public static void main(String[] var0) {
+       String var1 = "hello world";
+       System.out.println(var1.indexOf("w"));
+    }
+ }
+ 
