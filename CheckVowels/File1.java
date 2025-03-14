ee7c20483d320d419392d60aafdf7378981970ed
abishek2830 <l.s.abishek2021@gmail.com>
2025-03-14 11:11:36 +0530

    program'

--- CheckVowels/File1.java
+++ CheckVowels/File1.java
@@ -5,14 +5,21 @@ public class File1 {
         Scanner input= new Scanner(System.in);
         System.out.println("Enter the letter");
         String s=input.nextLine();
+        int detect=0;
         int i=0;
         while(i<5){
-            if(a[i]==s){
-                System.out.println("Its a Vowel");
-        }        
-            else{
-
-                       System.out.println("its not a vowel");
+            if(s.equals(a[i])){
+                detect = 1;
+            }
+            i=i+1;
         }
+        if(detect==1){
+            System.out.println("Its a Vowel");
+        }
+        else{
+            System.out.println("Its not a Vowel");
+        }
+
     }
 }
+
