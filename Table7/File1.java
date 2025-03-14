ece903d0b78ffb5c207eeb8c1f9e1b3b69b6b460
abishek2830 <l.s.abishek2021@gmail.com>
2025-03-14 10:14:07 +0530

    program'

---
+++ Table7/File1.java
@@ -0,0 +1,12 @@
+public class File1 {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=10){
+        System.out.println(7*i);
+        i++;
+
+        }
+    }
+
+    
+}
