2f32d64dfcbbf5ae10e95219b403594fa6615af7
abishek2830 <l.s.abishek2021@gmail.com>
2025-03-14 10:27:05 +0530

    program'

---
+++ Nnumbers/File1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class File1 {
+    public static void main(String[] args) {
+        int i=1;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n=input.nextInt();
+        while(i<=n)
+        {
+            System.out.println(i);
+        i++;
+    }
+    }
+
+}
