630a3444f9fce07297bcf79f194cd704f7ec4e4c
abishek2830 <l.s.abishek2021@gmail.com>
2025-03-15 09:43:42 +0530

    program

---
+++ CharAt/example.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class example{
+    public static void main(String args[]){
+        String n="hello";
+        System.out.println(n.charAt(0));
+    }
+}
---
+++ Split/example.java
@@ -0,0 +1,7 @@
+public class example{
+    public static void main(String args[]){
+        String n="hello,world";
+        String b[]=n.split(",");
+        System.out.println(b[0]);
+    }
+}
